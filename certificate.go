6b183a64e389652193bbd6999828827859488d2d
JackSparrow0009 <zh.luxu1986@gmail.com>
2022-07-15 03:27:04 +0000

    develop

--- certificate.go
+++ certificate.go
@@ -259,3 +259,27 @@ func RsaDecryption(
 ) ([]byte, error) {
 	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, ciphertext)
 }
+
+func chunkSplit(body string, chunklen uint, end string) string {
+	if end == "" {
+		end = "\r\n"
+	}
+
+	runes, erunes := []rune(body), []rune(end)
+	l := uint(len(runes))
+	if l <= 1 || l < chunklen {
+		return body + end
+	}
+
+	ns := make([]rune, 0, len(runes)+len(erunes))
+	var i uint
+	for i = 0; i < l; i += chunklen {
+		if i+chunklen > l {
+			ns = append(ns, runes[i:]...)
+		} else {
+			ns = append(ns, runes[i:i+chunklen]...)
+		}
+		ns = append(ns, erunes...)
+	}
+	return string(ns)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/JackSparrow0009/gocert
+
+go 1.17
+
+require (
+	github.com/andreburgaud/crypt2go v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
